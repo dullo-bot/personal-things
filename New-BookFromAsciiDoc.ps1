b0954b0da3d77ab9a6d0c20100efaabd5ab90a61
Thilo <thiloschein@web.de>
2021-06-26 17:37:33 +0200

    added command based help

--- New-BookFromAsciiDoc.ps1
+++ New-BookFromAsciiDoc.ps1
@@ -1,4 +1,31 @@
-function New-BookFromAsciiDoc {
+function New-BookFromAsciiDoc
+{
+    <#
+    .SYNOPSIS
+    this function creates a human-readable file from the Github Repos of "Mastering Bitcoin" and "Mastering Ethereum"
+    .DESCRIPTION
+    you can create either a .pdf or an .epub-File of the Open Source Versions of the books "Mastering Bitcoin" or "Mastering Ethereum"
+    the bitcoinbook-repo is a private fork, where I added some changes from open pull requests regarding formatting and wording
+    to use this function, you need asciidoctor, asciidoctor-pdf and asciidoctor-epub3, which you can find via https://asciidoctor.org
+    .EXAMPLE
+    New-BookFromAsciiDoc -Book BTC -Medium epub -verbose
+
+    Creates a .epub from "Mastering Bitcoin"
+    .EXAMPLE
+    New-BookFromAsciiDoc ETH pdf
+
+    Creates a .pdf from "Mastering Ethereum"
+    .PARAMETER Book
+    Specifies the preferred book
+    either BTC or ETH
+    .PARAMETER Medium
+    Specifies the preferred file extension
+    either epub or pdf
+    .NOTES
+    created by: dullo-bot
+    Date: 20210626
+    Tested with: MacOS 11.3; PS 7.1 BTC and epub
+    #>
     param (
         [Parameter(Mandatory,Position=0)]
         [ValidateSet("BTC","ETH")]
@@ -22,10 +49,6 @@ function New-BookFromAsciiDoc {
             {
                 Write-Verbose "Downloading the ethereumbook Repo"
                 Invoke-WebRequest https://github.com/ethereumbook/ethereumbook/archive/refs/heads/develop.zip -OutFile ./zipped.zip
-            }
-            Default
-            {
-
             }
         }
         Write-Verbose "expanding the zipped archive"
