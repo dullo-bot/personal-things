322a69cc516386e5ded4c4cdac80bf0b1075a6a4
Thilo <thiloschein@web.de>
2021-06-28 21:31:27 +0200

    rename function and v1 ready

---
+++ Remove-DuplicateFiles.ps1
@@ -0,0 +1,33 @@
+function Remove-DuplicateFiles {
+    <#
+    .SYNOPSIS
+    deletes duplicate files
+    .DESCRIPTION
+    this function removes all duplicate files.
+    It compares by the hashes and deletes the files found in the second folder
+    .EXAMPLE
+    Remove-DuplicateFiles -Path "C:/this/is/a/path"
+    .PARAMETER Path
+    The Path where duplicate files are located
+    .NOTES
+    Created by: dullo-bot
+    Date: 20210628
+    Tested with: Win10, PS 5.0
+    #>
+    
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Path = "C:\Users\Thilo\Documents\Bücher\HumbleBundle\"
+    )
+    $duplicateObjects = Get-ChildItem -Path $Path -Recurse | Get-FileHash | Group-Object -Property Hash | 
+    Where-Object Count -GT 1 | ForEach-Object
+    {
+        $_.Group | Select-Object Path, Hash
+    }
+    for ($i = 0; $i -lt $duplicateObjects.Count; $i++) {
+        if ($i % 2) {
+            Remove-Item -Path $duplicateObjects[$i].Path
+        }
+    }
+}
